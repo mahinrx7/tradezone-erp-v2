5d0b817342af33844ee0d44b7be6372f28fe2eee
mahinrx7 <mahie.ahmad05@gmail.com>
2026-05-11 12:48:15 +0300

    auto create admin

--- run.py
+++ run.py
@@ -1,11 +1,40 @@
 from app import create_app, db
 
+from app.models import User
+
+from flask_bcrypt import Bcrypt
+
+
 app = create_app()
+
+bcrypt = Bcrypt(app)
 
 
 with app.app_context():
 
     db.create_all()
+
+    # CREATE ADMIN IF NOT EXISTS
+    existing = User.query.filter_by(
+        username="mahmood"
+    ).first()
+
+    if not existing:
+
+        admin = User(
+
+            username="mahmood",
+
+            password=bcrypt.generate_password_hash(
+                "mahmood123"
+            ).decode("utf-8"),
+
+            role="admin"
+        )
+
+        db.session.add(admin)
+
+        db.session.commit()
 
 
 if __name__ == "__main__":
